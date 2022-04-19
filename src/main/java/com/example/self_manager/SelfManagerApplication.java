4fc60b0f7fe6600940e025417d42e36b225655cb
JEFFHSU\JeffHsu <jeff.hsu@promeritage.com.tw>
2022-04-19 09:28:14 +0800

    first commit

---
+++ src/main/java/com/example/self_manager/SelfManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.self_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/self_manager/SelfManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.self_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
