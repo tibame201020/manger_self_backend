3b0d10e2e1740886939606d7d7b35051991150b1
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2022-08-17 20:03:37 +0800

    Update LineChannelConfig.java

--- src/main/java/com/example/self_manager/config/LineChannelConfig.java
+++ src/main/java/com/example/self_manager/config/LineChannelConfig.java
@@ -6,7 +6,7 @@ public class LineChannelConfig {
     public static final String CHANNEL_ID_SECRET = "7461f90ef413c1969b01cfe4e7bb14c4";
     public static final String BASE_ACCESS_URL = "https://access.line.me/oauth2/v2.1/authorize";
     public static final String QUERY_PARAM = "?response_type=code&client_id=%s&redirect_uri=%s&state=%s&scope=%s";
-    public static final String REDIRECT_URI = "http://localhost:4200/user/login";
+    public static final String REDIRECT_URI = "https://tibame201020.github.io/manager_self/user/login";
     public static final String GET_TOKEN_URL = "https://api.line.me/oauth2/v2.1/token";
 
     public static String getAccessUrl () {
